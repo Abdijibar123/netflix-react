9ed35b4396b9db908ba9f01e122992b54dacef1c
Abdijibar123 <abdijibar1313@gmail.com>
2023-08-07 22:13:20 +0300

    new react netflix

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,16 +1,16 @@
-function MovieCard() {
-  return (
-    <div className="movie-card">
-      <div className="movie-image">
-        <img
-          src="https://i.pinimg.com/originals/a2/34/75/a234753b69e3bd27b7f1d448956c38af.jpg"
-          alt="movie"
-        />
-      </div>
+import React from "react";
 
-      <h3 className="movie-heading">Breaking Bad</h3>
-    </div>
-  );
-}
+function MovieCard({ movie }) {
+return (
 
+<div className="movie-card"> <div className="movie-image"> 
+<img src={movie.show.image.medium} alt="movie" /> </div>
+
+<h3 className="movie-heading">{movie.show.name}</h3>
+ <p>⭐ {movie.show.rating.average || 0}</p>
+  <ul className="list"> {movie.show.genres.map((genre) => 
+  ( <li key={genre}> | {genre}</li> ))}
+   </ul> <button>
+     <a href={movie.show.url} target="_blank" rel="noopener noreferrer" className="website-button">
+     Website </a> </button> </div> ); }
 export default MovieCard;
